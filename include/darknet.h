b4d42a4fa8a3b5232ddac64870bf2d3795a4d87b
Sondre Russvoll <sondre@russvoll.no>
2018-04-27 12:26:52 +0200

    Fixed global variable in header.

--- include/darknet.h
+++ include/darknet.h
@@ -673,7 +673,7 @@ image *get_weights(layer l);
 void demo(char *cfgfile, char *weightfile, float thresh, int cam_index, const char *filename, char **names, int classes, int frame_skip, char *prefix, int avg, float hier_thresh, int w, int h, int fps, int fullscreen, int every, int threads);
 void get_detection_detections(layer l, int w, int h, float thresh, detection *dets);
 
-char* globalBackupDir;
+extern char* globalBackupDir;
 
 char *option_find_str(list *l, char *key, char *def);
 int option_find_int(list *l, char *key, int def);
